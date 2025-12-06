1808e9b408513142f6fee2ed7c41976ca0c63945
biamelo0205-lgtm <biamelo0205@gmail.com>
2025-12-06 02:55:26 +0000

    Primeiro commit do tutorial

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Projeto-Curso 2025")
+st.header("Mulheres na Ciência.",divider=True )
+st.subheader("Meninas apaixonadas por tecnologia aprendendo a programar.")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
